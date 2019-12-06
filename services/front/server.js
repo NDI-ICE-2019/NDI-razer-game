584e8deefcbc3ca8a7a58afb65d7a19e70e6cf38
Matthieu Orriere <matthieu.orriere@gmail.com>
2019-12-06 02:48:43 +0100

    FRONT : Trying to communicate with back

--- services/front/server.js
+++ services/front/server.js
@@ -1,12 +1,22 @@
 var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var path = require('path');
 
 app.use(express.static("src/component"));
 
-// viewed at http://localhost:8080
-app.get('/', function(req, res) {
-    res.sendFile(path.resolve(__dirname, "./src/component/index.html"))
+server.listen(80);
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, "./src/component/index.html"));
 });
 
+io.on('chat message', function (socket) {
+  console.log(socket);
+});
+
+io.emit('test');
 app.listen(8080);
+  
